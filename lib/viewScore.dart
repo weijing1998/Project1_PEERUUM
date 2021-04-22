f60068ef52b7a84413f74d89b2bc7f06da485052
weijing1998 <weijing980924@gmail.com>
2021-04-23 04:15:16 +0800

    uodate

--- lib/viewScore.dart
+++ lib/viewScore.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:pepelist/objects/course.dart';
 import 'package:pepelist/objects/form.dart';
@@ -175,7 +177,7 @@ class _ViewScoreState extends State<ViewScore> {
                       padding: const EdgeInsets.fromLTRB(160, 10, 10, 20),
                       child: Align(
                         child: Text(
-                          "STUDENT PERFORMANCE",
+                          "STUDENT PERFORMANCE & COMMENTS",
                           style: TextStyle(
                               fontSize: 30, fontWeight: FontWeight.bold),
                         ),
@@ -258,7 +260,91 @@ class _ViewScoreState extends State<ViewScore> {
                     ),
 
                     SizedBox(
-                      height: size.height / 4,
+                      height: size.height / 8,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(160, 10, 10, 20),
+                      child: Align(
+                        child: Text(
+                          "STUDENT TOTAL SCORE",
+                          style: TextStyle(
+                              fontSize: 30, fontWeight: FontWeight.bold),
+                        ),
+                        alignment: Alignment.centerLeft,
+                      ),
+                    ),
+
+                    SingleChildScrollView(
+                      child: Container(
+                        width: size.width / 1.5,
+                        color: Colors.grey[300],
+                        child: snapshotCourse.data.listOfForm.length != 0 &&
+                                snapshotCourse.data.listOfScore.length != 0
+                            ? ListView.builder(
+                                shrinkWrap: true,
+                                itemCount:
+                                    snapshotCourse.data.listOfForm.length,
+                                itemBuilder: (context, index) {
+                                  return Card(
+                                    child: Padding(
+                                      padding: EdgeInsets.all(4.0),
+                                      child: Container(
+                                        width: double.infinity,
+                                        color: Colors.teal[200],
+                                        child: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Container(
+                                              color: Colors.teal[600],
+                                              width: double.infinity,
+                                              child: Padding(
+                                                padding:
+                                                    const EdgeInsets.all(18.0),
+                                                child: Text(
+                                                  "Form Name : " +
+                                                      snapshotCourse.data
+                                                              .listOfForm[index]
+                                                          ["formname"],
+                                                  style: TextStyle(
+                                                      fontSize: 23,
+                                                      fontWeight:
+                                                          FontWeight.w800,
+                                                      color: Colors.white),
+                                                ),
+                                              ),
+                                            ),
+                                            Container(
+                                              height: size.height / 1.7,
+                                              width: double.infinity,
+                                              child: TotalScoreWidget(
+                                                listofscore: snapshotCourse
+                                                    .data.listOfScore,
+                                                forms: Forms.fromJson(
+                                                    snapshotCourse.data
+                                                        .listOfForm[index]),
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  );
+                                },
+                              )
+                            : Center(
+                                child: Container(
+                                    height: size.height / 5,
+                                    width: size.width / 3,
+                                    child: Center(
+                                      child: Text(
+                                        "No Form Applied or No Student Submit Any Evaluation",
+                                        style: TextStyle(
+                                            fontSize: 20,
+                                            fontWeight: FontWeight.w500),
+                                      ),
+                                    ))),
+                      ),
                     ),
                   ],
                 ),
@@ -288,7 +374,6 @@ class ScoreWidget extends StatefulWidget {
 }
 
 class _ScoreWidgetState extends State<ScoreWidget> {
-  @override
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -527,5 +612,196 @@ class _ScoreWidgetState extends State<ScoreWidget> {
         builder: (context) {
           return alert;
         });
+  }
+}
+
+class TotalScoreWidget extends StatefulWidget {
+  final List listofscore;
+  final Forms forms;
+
+  TotalScoreWidget({Key key, @required this.listofscore, @required this.forms})
+      : super(key: key);
+
+  @override
+  _TotalScoreWidgetState createState() => _TotalScoreWidgetState();
+}
+
+class _TotalScoreWidgetState extends State<TotalScoreWidget> {
+  List sameForm = [];
+  List totalscorewithduplicate = [];
+  List totalscore = [];
+  @override
+  void initState() {
+    for (int i = 0; i < widget.listofscore.length; i++) {
+      if (widget.listofscore[i]["formid"] == widget.forms.formID) {
+        sameForm.add(widget.listofscore[i]);
+      }
+    }
+
+    for (int i = 0; i < sameForm.length; i++) {
+      int totalpoint = 0;
+      int divide = 0;
+
+      for (int j = 0; j < sameForm.length; j++) {
+        if (sameForm[i]["evaluateuseremail"] ==
+            sameForm[j]["evaluateuseremail"]) {
+          totalpoint = totalpoint + sameForm[j]["studentpoint"];
+          divide = divide + 1;
+        }
+      }
+
+      Map map = {
+        "email": sameForm[i]["evaluateuseremail"],
+        "name": sameForm[i]["evaluateusername"],
+        "matric": sameForm[i]["evaluateusermatric"],
+        "totalscore": totalpoint / divide,
+      };
+      totalscorewithduplicate.add(map);
+    }
+
+    final jsonList =
+        totalscorewithduplicate.map((item) => jsonEncode(item)).toList();
+    final uniqueJsonList = jsonList.toSet().toList();
+    final result = uniqueJsonList.map((item) => jsonDecode(item)).toList();
+    totalscore = result;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return ListView.builder(
+      itemCount: totalscore.length,
+      itemBuilder: (context, index) {
+        return Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Container(
+              color: Colors.white54,
+              height: size.height / 6,
+              width: double.infinity,
+              child: Row(
+                children: [
+                  SizedBox(
+                    width: size.width / 25,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: CircleAvatar(
+                      backgroundImage: NetworkImage('images/header.jpg'),
+                      radius: 45,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 30, vertical: 10),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: Text(
+                            "Name :" +
+                                widget.listofscore[index]["evaluateusername"],
+                            style: TextStyle(fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: Text(
+                            "Email :" +
+                                widget.listofscore[index]["evaluateuseremail"],
+                            style: TextStyle(fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: Text(
+                            "Email :" +
+                                widget.listofscore[index]["evaluateusermatric"],
+                            style: TextStyle(fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: Text(
+                            "Group :" + widget.listofscore[index]["groupname"],
+                            style: TextStyle(fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: Text(
+                            "Student point :" +
+                                widget.listofscore[index]["studentpoint"]
+                                    .toString(),
+                            style: TextStyle(fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: Text(
+                            "Total point :" +
+                                widget.listofscore[index]["totalpoint"]
+                                    .toString(),
+                            style: TextStyle(fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: Text(
+                            "Rate By :" +
+                                widget.listofscore[index]["currentuser"],
+                            style: TextStyle(fontWeight: FontWeight.w500),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    width: size.width / 18,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Center(
+                      child: Text(
+                        calculatepercentage(
+                                    widget.listofscore[index]["studentpoint"],
+                                    widget.listofscore[index]["totalpoint"])
+                                .toString() +
+                            "%",
+                        style: TextStyle(
+                          fontSize: 25,
+                          color: Colors.blue,
+                          fontFamily: "Dela Gothic One",
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: size.width / 13,
+                  ),
+                ],
+              ),
+            ));
+      },
+    );
+  }
+
+  double calculatepercentage(int studentpoint, int totalpoint) {
+    double percentage = (studentpoint / totalpoint) * 100;
+    String parse = percentage.toStringAsFixed(2);
+    double afterparse = double.parse(parse);
+    return afterparse;
   }
 }
